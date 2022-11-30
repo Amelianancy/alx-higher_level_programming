caff245e90332ca77ce44767700e056ffb820cc1
Amelianancy <ogbonnanancy11@gmail.com>
2022-11-30 21:49:24 +0000

    100-print

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
-
-for i in range(ord('z'), ord('a') - 1, - 1):
-    if i % 2 != 0:
-        i -= 32
-    print("{}".format(chr(i)), end="")
+for i in reversed(range(97, 123)):
+    if (i % 2 == 0):
+        print('{:c}'.format(i), end='')
+    else:
+        print('{:c}'.format(i - 32), end='')
