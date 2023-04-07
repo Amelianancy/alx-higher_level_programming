52bb6c3cd933dc15178a058d97064460a3a127d8
Amelianancy <ogbonnanancy11@gmail.com>
2023-04-07 00:56:17 -0700

    all done

---
+++ 0x11-python-network_1/10-my_github.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""
+takes a github username and password and uses the github API to display the id"""
+
+
+import requests
+import sys
+
+if __name__ == "__main__":
+    username = sys.argv[1]
+    password = sys.argv[2]
+    r = requests.get('https://api.github.com/user', auth=(username, password))
+    response = r.json()
+    print(response.get('id'))
